006acbfbfcc0449e3883d9439dbaffa7f4c864b1
jgwash08 <washij2@mail.uc.edu>
2026-09-24 21:49:34 -0400

    Yearly Maintenance Cost

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+void main() {
+//Declarations
+    double springCost = 150.00;
+    double summerCost = 250.00;
+    double fallCost = 190.00;
+    double winterCost = 350.00;
+    double totalYearlyCost = springCost + summerCost + fallCost + winterCost;
+
+    // Displaying seasonal costs and total
+    System.out.println("Spring Maintenance Cost: $" + springCost);
+    System.out.println("Summer Maintenance Cost: $" + summerCost);
+    System.out.println("Fall Maintenance Cost: $" + fallCost);
+    System.out.println("Winter Maintenance Cost: $" + winterCost);
+    System.out.println("Total Yearly Maintenance Cost: $" + totalYearlyCost);
+}
+
+}
